6da61e2317edd2b5884d41b9f7c84c1be4a32dbc
josh <joshlagrimas1808@gmail.com>
2025-07-28 01:05:12 +0800

    fix: added comments in db config

--- server/app/db.config.py
+++ server/app/db.config.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base 
 from settings import settings
 
-engine = create_engine(settings.DB_URL, echo=settings.DEBUG) #sets up the connection to the db
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) #interface tot alk to db, lets add, query, update, delete
-Base = declarative_base #base class for ORM models (Object Relational Mapping lets you work with db using python classes and obj)
+#sets up the connection to the db
+engine = create_engine(settings.DB_URL, echo=settings.DEBUG) 
+#interface to talk to db, lets add, query, update, bind=engine connects session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
+#base class for ORM models (Object Relational Mapping lets you work with db using python classes and obj)
+Base = declarative_base 
