122d631336679ee1f7763f1f1901c52d4b6d7e8f
josh <joshlagrimas1808@gmail.com>
2025-07-27 18:54:35 +0800

    practice using path and query parameters

--- server/app/main.py
+++ server/app/main.py
@@ -2,18 +2,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+class Item(BaseModel):
+    name: int
+    description: int = None
+    price: float
+    tax: float = None
 
 @app.get("/")
 def read_root():
     return {"Hello" : "World"}
 
-class Item(BaseModel):
-    name: str
-    description: str = None
-    price: float
-    tax: float = None
-
-@app.post("/items/")
-def create_item(item:Item):
-    return item
+@app.get("/items/{item_id}")
+def read_item(item_id: str, q: str | None = None, sample: bool = False):
+    item = {"item_id": item_id}
+    item.update({"q": q})
+    return {item}
 
