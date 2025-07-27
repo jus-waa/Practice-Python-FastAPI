3dfc4a70f46f89b8ed4ca818483b9b05e8b567ed
josh <joshlagrimas1808@gmail.com>
2025-07-28 01:07:11 +0800

    fix: db config

--- server/app/db.config.py
+++ server/app/db.config.py
@@ -8,4 +8,4 @@ engine = create_engine(settings.DB_URL, echo=settings.DEBUG)
 #interface to talk to db, lets add, query, update, bind=engine connects session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
 #base class for ORM models (Object Relational Mapping lets you work with db using python classes and obj)
-Base = declarative_base 
+Base = declarative_base()
